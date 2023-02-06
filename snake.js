9be59d1ae614e0301c0c4d378a0223fded5c815e
lcastienc <lcastill@jaumebalmes.net>
2023-02-06 20:28:11 +0100

    Creació canvas i dibuix del mateix

--- snake.js
+++ snake.js
@@ -3,97 +3,102 @@
  * @class
  */
 class Game {
+  /**
+   * Inicialitza els paràmetres del joc i crea el canvas
+   * @constructor
+   * @param {number} width -  width del canvas
+   * @param {number} height -  height del canvas
+   * @param {number} amount -  nombre de quadrats per fila de la quadrícula
+   */
+  constructor(width, height, amount) {
+    this.width = width;
+    this.height = height;
+    this.amount = amount;
+    //llamamos a las funciones para comenzar el jeugo junto al canvas
+    //al canvas se le pasan el width y height para su forma
+    this.initCanvas(width, height);
+    this.start();
+  }
 
-	/**
-	 * Inicialitza els paràmetres del joc i crea el canvas
-	 * @constructor
-	 * @param {number} width -  width del canvas
-	 * @param {number} height -  height del canvas
-	 * @param {number} amount -  nombre de quadrats per fila de la quadrícula
-	 */
-	constructor(width,height,amount) {
-	}
+  /**
+   * Crea un canvas i es guarda el [context](https://developer.mozilla.org/es/docs/Web/API/CanvasRenderingContext2D) a un atribut per poder
+   * accedir-hi des dels mètodes de pintar al canvas (com ara drawSquare, clear)
+   * @param {number} width -  width del canvas
+   * @param {number} height -  height del canvas
+   */
+  initCanvas(width, height) {
+    //Creamos el canvas
+    let canvas = document.createElement("canvas");
+    //Damos los valores introducidos al canvas
+    canvas.width = width;
+    canvas.height = height;
+    //Añadimos borde al canvas para que se vea
+    canvas.style.border = "3px solid";
+    //Añadimos el canvas en primer elemento de body, por eso el [0]
+    document.getElementsByTagName("body")[0].appendChild(canvas);
+  }
 
-	/**
-	 * Crea un canvas i es guarda el [context](https://developer.mozilla.org/es/docs/Web/API/CanvasRenderingContext2D) a un atribut per poder
-	 * accedir-hi des dels mètodes de pintar al canvas (com ara drawSquare, clear)
-	 * @param {number} width -  width del canvas
-	 * @param {number} height -  height del canvas
-	 */
-	initCanvas(width, height) {
-	}
+  /**
+   * Inicialitza els paràmetres del joc:
+   * Serp al centre, direcció cap a la dreta, puntuació 0
+   */
+  start() {}
 
-	/**
-	 * Inicialitza els paràmetres del joc:
-	 * Serp al centre, direcció cap a la dreta, puntuació 0
-	 */
-	start() {
-	}
+  /**
+   * Dibuixa un quadrat de la mida de la quadrícula (passada al constructor) al canvas
+   * @param {number} x -  posició x de la quadrícula (no del canvas)
+   * @param {number} y -  posició y de la quadrícula (no del canvas)
+   * @param {string} color -  color del quadrat
+   */
+  drawSquare(x, y, color) {}
 
-	/**
-	 * Dibuixa un quadrat de la mida de la quadrícula (passada al constructor) al canvas
-	 * @param {number} x -  posició x de la quadrícula (no del canvas)
-	 * @param {number} y -  posició y de la quadrícula (no del canvas)
-	 * @param {string} color -  color del quadrat
-	 */
-	drawSquare(x,y,color) {
-	}
+  /**
+   * Neteja el canvas (pinta'l de blanc)
+   */
+  clear() {}
 
-	/**
-	 * Neteja el canvas (pinta'l de blanc)
-	 */
-	clear() {
-	}
+  /**
+   * Dibuixa la serp al canvas
+   */
+  drawSnake() {}
 
-	/**
-	 * Dibuixa la serp al canvas
-	 */
-	drawSnake() {
-	}
+  /**
+   * Dibuixa la poma al canvas
+   */
+  drawFood() {}
 
-	/**
-	 * Dibuixa la poma al canvas
-	 */
-	drawFood() {
-	}
+  /**
+   * La serp xoca amb la posició donada?
+   * @param {number} x -  posició x a comprovar
+   * @param {number} y -  posició y a comprovar
+   * @return {boolean} - xoca o no
+   */
+  collides(x, y) {}
 
-	/**
-	 * La serp xoca amb la posició donada?
-	 * @param {number} x -  posició x a comprovar
-	 * @param {number} y -  posició y a comprovar
-	 * @return {boolean} - xoca o no
-	 */
-	collides(x,y) {
-	}
+  /**
+   * Afegeix un menjar a una posició aleatòria, la posició no ha de ser cap de les de la serp
+   */
+  addFood() {}
 
-	/**
-	 * Afegeix un menjar a una posició aleatòria, la posició no ha de ser cap de les de la serp
-	 */
-	addFood() {
-	}
+  /**
+   * Calcula una nova posició a partir de la ubicació de la serp
+   * @return {Array} - nova posició
+   */
+  newTile() {}
 
-	/**
-	 * Calcula una nova posició a partir de la ubicació de la serp
-	 * @return {Array} - nova posició
-	 */
-	newTile() {
-	}
+  /**
+   * Calcula el nou estat del joc, nova posició de la serp, nou menjar si n'hi ha ...
+   * i ho dibuixa al canvas
+   */
+  step() {}
 
-	/**
-	 * Calcula el nou estat del joc, nova posició de la serp, nou menjar si n'hi ha ...
-	 * i ho dibuixa al canvas
-	 */
-	step() {
-	}
-
-	/**
-	 * Actualitza la direcció de la serp a partir de l'event (tecla dreta, esquerra, amunt, avall)
-	 * @param {event} e - l'event de la tecla premuda
-	 */
-	input(e) {
-	}
+  /**
+   * Actualitza la direcció de la serp a partir de l'event (tecla dreta, esquerra, amunt, avall)
+   * @param {event} e - l'event de la tecla premuda
+   */
+  input(e) {}
 }
 
-let game = new Game(300,300,15); // Crea un nou joc
+let game = new Game(300, 300, 15); // Crea un nou joc
 document.onkeydown = game.input.bind(game); // Assigna l'event de les tecles a la funció input del nostre joc
-window.setInterval(game.step.bind(game),100); // Fes que la funció que actualitza el nostre joc s'executi cada 100ms
+window.setInterval(game.step.bind(game), 100); // Fes que la funció que actualitza el nostre joc s'executi cada 100ms
